6142f7b8e74121814fe9c933fe4c2c3a47b57c9c
Julian <espitiadevi@gmail.com>
2026-08-24 09:22:18 -0500

    Actualizar archivo con Reto 3#

--- practica-js.js
+++ practica-js.js
@@ -74,3 +74,18 @@ const nombreCompleto = (nombre,apellido) =>{
     return nombre + " " + apellido;
 }
 console.log(nombreCompleto("Julian","Espitia"))
+
+//Reto 3#:
+const productos = [
+{ id: 1, nombre: 'Mouse', precio: 50000, categoria: 'Perifericos', stock: 5 },
+{ id: 2, nombre: 'Teclado', precio: 90000, categoria: 'Perifericos', stock: 0 },
+{ id: 3, nombre: 'Monitor', precio: 650000, categoria: 'Pantallas', stock: 3 },
+{ id: 4, nombre: 'Mesa', precio: 340000, categoria: 'Muebles', stock: 2 },
+{ id: 5, nombre: 'Silla', precio: 32000, categoria: 'Muebles', stock: 1 },
+{ id: 6, nombre: 'Sofa', precio: 123000, categoria: 'Muebles', stock: 4 },
+{ id: 7, nombre: 'Audifonos', precio: 14000, categoria: 'Perifericos', stock: 0 },
+{ id: 8, nombre: 'Lampara', precio: 70000, categoria: 'Decoracion', stock: 7 },
+{ id: 9, nombre: 'Cuadro', precio: 200000, categoria: 'Decoracion', stock: 8 },
+{ id: 10, nombre: 'Destornillador', precio: 20000, categoria: 'Herramientas', stock: 9 }
+];
+
