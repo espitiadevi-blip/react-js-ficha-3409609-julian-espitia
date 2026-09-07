4bb257eee67a669e985cc01f4508fd21afcc352b
--global <farfanlaura016@gmail.com>
2026-09-07 09:13:27 -0500

    Agrege NuevoProducto

--- src/pages/NuevoProducto.jsx
+++ src/pages/NuevoProducto.jsx
@@ -1,4 +1,32 @@
+import { useNavigate } from "react-router-dom";
+import FormularioProducto from "../components/FormularioProducto";
+
 function NuevoProducto() {
-  return <h1>Nuevo Producto</h1>;
+  const navigate = useNavigate();
+
+  const agregarProducto = (nuevoProducto) => {
+    const productosGuardados =
+      JSON.parse(localStorage.getItem("productos")) || [];
+
+    productosGuardados.push(nuevoProducto);
+
+    localStorage.setItem(
+      "productos",
+      JSON.stringify(productosGuardados)
+    );
+
+    navigate("/inventario");
+  };
+
+  return (    
+    <section>
+      <h1>Nuevo Producto</h1>
+
+      <FormularioProducto
+        onAgregar={agregarProducto}
+      />
+    </section>
+  );
 }
+
 export default NuevoProducto;
